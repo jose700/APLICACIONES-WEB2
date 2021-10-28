8b5b3fee40ec19511d2ad95d274bcde6f6584f65
jose700 <josecampu28@gmail.com>
2021-10-28 12:42:40 -0500

    PRUEBA

--- introduccion/conexion.js
+++ introduccion/conexion.js
@@ -3,13 +3,22 @@ const express = require('express');
 
 const morgan = require("morgan");
 const mongoose = require('mongoose');
-
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 //conexion a la base de datos
-mongoose.connect('mongodb://localhost/LUIS').then(db => (
+mongoose.connect('mongodb://localhost/prueba').then(db => (
     console.log('te has conectado a la base de datos')
 )).catch(err => console.log('error de conexion', err));
 
+// modelo
+const Usuario = mongoose.model("Usuario",{nombre:String});
+const Usuario1 = new Usuario({nombre:"Josè"});
+Usuario1.save();
+
+// consultar el dato añadido en la db
+
+Usuario.find().then(console.log)
 // configuracion de las vistas
 app.set('views', path.join(__dirname, 'view'));
 app.set('views engine', 'ejs');
