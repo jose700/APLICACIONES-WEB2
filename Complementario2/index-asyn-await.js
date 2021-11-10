67d4b70d1802ea066f0ad6afcc16577588b33645
jose700 <josecampu28@gmail.com>
2021-11-09 20:12:19 -0500

    PRUEBA

--- Complementario2/index-asyn-await.js
+++ Complementario2/index-asyn-await.js
@@ -1,21 +1,19 @@
-
-const malla = [
-    {
+const malla = [{
         id: 1,
-        Ti:{
+        Ti: {
             Nivel1: "Fisica, Aprendizaje a la comunicacion Humana, Metodologia de la Investigacion",
             Nivel2: "Calculo, Estructuta de Datos, Estadistica y Probabilidad",
             Nivel3: "Ecuaciones Diferenciales"
 
         },
         id_docentes: 1
-      
+
 
 
     },
     {
         id: 2,
-        sistemas:{
+        sistemas: {
             Nivel1: "Fisica, Sistemas electricos, Laica",
             Nivel2: "Calculo, Estructuta de Datos, Estadistica y Probabilidad",
             Nivel3: "Ecuaciones Diferenciales"
@@ -25,12 +23,11 @@ const malla = [
 
     }
 
-]
+];
 
-const docentes = [
-    {
-        id:1,
-        docentes_Ti:{
+const docentes = [{
+        id: 1,
+        docentes_Ti: {
             Nivel1: "Ing Elsa",
             Nivel2: "Walter",
             Nivel3: "Alfredo"
@@ -39,8 +36,8 @@ const docentes = [
 
     },
     {
-        id:2,
-        docentes_sistemas:{
+        id: 2,
+        docentes_sistemas: {
             Nivel1: "Hiraira",
             Nivel2: "Jhon Cevallos",
             Nivel3: "RAUL"
@@ -48,66 +45,66 @@ const docentes = [
         id_silabos: 2
     }
 
-]
-const silabos = [
-    {
-        id:1,
+];
+const silabos = [{
+        id: 1,
         Silabosistemas: "silaboSitemas.pdf",
-        
+
     },
     {
-        id:2,
+        id: 2,
         silaboTI: "SilaboIngTi.pdf",
-        
+
     }
 
-]
-async function buscarMallaPorId(id){
-   
+];
+async function buscarMallaPorId(id) {
 
-        const mallas = malla.find(m=> m.id === id)
-        if(!mallas){
 
-            const e = new Error();
-            e.message = "NO ENCONTRADO"
-            throw e;
-        }
-        return mallas;
-    
-}
+    const mallas = malla.find(m => m.id === id);
+    if (!mallas) {
 
-async function buscarDocentesPorId(id){
-    
-
-        const docente = docentes.find(m=> m.id === id)
-        if(!docente){
-
-            const e = new Error();
-            e.message = "NO ENCONTRADO"
-            throw e
-        }
-
-        return docente;
-
-    
-}
-function buscarSilabosPorId(id){
-    
-        const silabo = silabos.find(m=> m.id === id)
-        if(!silabo){
-            const e = new Error();
-            e.message = "NO ENCONTRADO"
-            throw e
-        }
-        return silabo;
-
-    
+        const e = new Error();
+        e.message = "NO ENCONTRADO";
+        throw e;
+    }
+    return mallas;
 
 }
 
-async function main(){
+async function buscarDocentesPorId(id) {
 
-    try{
+
+    const docente = docentes.find(m => m.id === id);
+    if (!docente) {
+
+        const e = new Error();
+        e.message = "NO ENCONTRADO";
+        throw e;
+    }
+
+    return docente;
+
+
+}
+
+function buscarSilabosPorId(id) {
+
+    const silabo = silabos.find(m => m.id === id);
+    if (!silabo) {
+        const e = new Error();
+        e.message = "NO ENCONTRADO";
+        throw e;
+    }
+    return silabo;
+
+
+
+}
+
+async function main() {
+
+    try {
         const malla = await buscarMallaPorId(1);
         const docente = await buscarDocentesPorId(malla.id_docentes);
         const silabo = await buscarSilabosPorId(docente.id_silabos);
@@ -115,9 +112,9 @@ async function main(){
         console.log(docente);
         console.log(silabo);
 
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
     }
- 
+
 }
 main();
--- Complementario2/indexPromise.js
+++ Complementario2/indexPromise.js
@@ -1,18 +1,17 @@
-const malla = [
-    {
+const malla = [{
         id: 1,
-        Ti:{
+        Ti: {
             Nivel1: "Fisica, Aprendizaje a la comunicacion Humana, Metodologia de la Investigacion",
             Nivel2: "Calculo, Estructuta de Datos, Estadistica y Probabilidad",
             Nivel3: "Ecuaciones Diferenciales"
 
         },
         id_docentes: 1
-      
+
     },
     {
         id: 2,
-        sistemas:{
+        sistemas: {
             Nivel1: "Fisica, Sistemas electricos, Laica",
             Nivel2: "Calculo, Estructuta de Datos, Estadistica y Probabilidad",
             Nivel3: "Ecuaciones Diferenciales"
@@ -22,12 +21,11 @@ const malla = [
 
     }
 
-]
+];
 
-const docentes = [
-    {
-        id:1,
-        docentes_Ti:{
+const docentes = [{
+        id: 1,
+        docentes_Ti: {
             Nivel1: "Ing Elsa",
             Nivel2: "Walter",
             Nivel3: "Alfredo"
@@ -36,8 +34,8 @@ const docentes = [
 
     },
     {
-        id:2,
-        docentes_sistemas:{
+        id: 2,
+        docentes_sistemas: {
             Nivel1: "Hiraira",
             Nivel2: "Jhon Cevallos",
             Nivel3: "RAUL"
@@ -45,83 +43,82 @@ const docentes = [
         id_silabos: 2
     }
 
-]
-const silabos = [
-    {
-        id:1,
+];
+const silabos = [{
+        id: 1,
         Silabosistemas: "silaboSitemas.pdf",
-        
+
     },
     {
-        id:2,
+        id: 2,
         silaboTI: "SilaboIngTi.pdf",
-        
+
     }
 
-]
+];
 
 
 
-function buscarMallaPorId(id){
-    return new Promise ((resolve, reject)=>{
+function buscarMallaPorId(id) {
+    return new Promise((resolve, reject) => {
 
-        const mallas = malla.find(m=> m.id === id)
-        if(!mallas){
+        const mallas = malla.find(m => m.id === id);
+        if (!mallas) {
 
             const e = new Error();
-            e.message = "NO ENCONTRADO"
-            reject(e)
+            e.message = "NO ENCONTRADO";
+            reject(e);
         }
 
-            resolve(mallas)
+        resolve(mallas);
 
-    })
+    });
 }
 
 
-buscarMallaPorId(1).then(malla=> {
-    
-    console.log(malla)
-    return buscarDocentesPorId(malla.id_docentes).then(malla =>{
-        console.log(malla)
-     
-    })
+buscarMallaPorId(1).then(malla => {
+
+    console.log(malla);
+    return buscarDocentesPorId(malla.id_docentes).then(malla => {
+        console.log(malla);
+
+    });
 
 
 })
 
 
-.catch(error=> console.log(error));
+.catch(error => console.log(error));
 
 
 
-function buscarDocentesPorId(id){
-    return new Promise ((resolve, reject)=>{
+function buscarDocentesPorId(id) {
+    return new Promise((resolve, reject) => {
 
-        const docente = docentes.find(m=> m.id === id)
-        if(!docente){
+        const docente = docentes.find(m => m.id === id);
+        if (!docente) {
 
             const e = new Error();
-            e.message = "NO ENCONTRADO"
-            reject(e)
+            e.message = "NO ENCONTRADO";
+            reject(e);
         }
 
-            resolve(docente)
+        resolve(docente);
 
-    })
+    });
 }
-function buscarSilabosPorId(id){
-    return new Promise ((resolve, reject)=>{
-        const silabo = silabos.find(m=> m.id === id)
-        if(!silabo){
+
+function buscarSilabosPorId(id) {
+    return new Promise((resolve, reject) => {
+        const silabo = silabos.find(m => m.id === id);
+        if (!silabo) {
             const e = new Error();
-            e.message = "NO ENCONTRADO"
-            reject(e)
+            e.message = "NO ENCONTRADO";
+            reject(e);
         }
-        resolve(silabo)
+        resolve(silabo);
 
-    })
+    });
 
 }
-buscarSilabosPorId(1).then(w => console.log(w)).catch(err=> console.log(err));
-
+buscarSilabosPorId(1).then(w => console.log(w)).catch(err => console.log(err));
