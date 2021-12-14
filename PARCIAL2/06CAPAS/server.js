d0beef65a42adb4f359d8e56195fc86b2c1cff4a
jose700 <josecampu28@gmail.com>
2021-12-14 11:01:33 -0500

    docker pruebas

---
+++ PARCIAL2/06CAPAS/server.js
@@ -0,0 +1,58 @@
+/// CREAR SERVIDOR DE EXPRESS
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+
+
+
+class Server {
+    constructor() {
+            this.app = express.Router();
+            this.router = express.Router();
+
+            this.port = process.env.PORT;
+
+            this.paths = {
+                productos: '/api/productos',
+                grupos: '/api/grupos',
+                categorias: '/api/categorias'
+            };
+
+            this.conectarDB();
+            this.middlewares();
+            this.routes();
+
+            this.router.use('/v1/pruebas', this.app);
+
+
+            this._express = express().use(this.router);
+
+        }
+        //// ASOCIAR RUTAS, MIDDLEWARES, LEVANTAR BASE DE DATOS
+    async conectarDB() {
+        await dbConnection();
+    }
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+
+    }
+    routes() {
+        this.app.use(this.paths.productos, require('./routes/productos'));
+        this.app.use(this.paths.categorias, require('./routes/categorias'));
+
+    }
+
+    listen() {
+        this._express.listen(this.port, () => {
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        });
+
+    }
+
+}
+
+
+module.exports = Server;
